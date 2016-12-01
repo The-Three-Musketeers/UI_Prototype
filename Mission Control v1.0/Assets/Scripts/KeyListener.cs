b441c62b87c1749fb64910420aec8c75438ad4a7
KRader627 <klr020@bucknell.edu>
2016-12-01 16:36:00 -0500

    Initial commit
    Let's not screw this up this time

---
+++ Mission Control v1.0/Assets/Scripts/KeyListener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyListener : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q)){
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            if (Application.loadedLevel != 0) {
+                Application.LoadLevel(Application.loadedLevel - 1);
+            }
+        }
+	}
+}
