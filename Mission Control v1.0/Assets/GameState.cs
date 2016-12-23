92e748d30e351cebc1c9b782f39473e1d3343363
KRader627 <klr020@bucknell.edu>
2016-12-23 15:19:40 -0500

    Updated Sprites and implemented color changing rocket
    Added new sprites congruent with what we talked about in our last
    meeting. Additionally, I implemented the ability for the rocket to
    change color on the user's choices in full.

---
+++ Mission Control v1.0/Assets/GameState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	public static string mission;
+	public static string rocket_color;
+
+	public string get_mission() {
+		return mission;
+	}
+
+	public void set_mission(string new_mission) {
+		mission = new_mission;
+	}
+
+	public string get_rocket_color() {
+		return rocket_color;
+	}
+
+	public void set_rocket_color(string new_rocket_color) {
+		rocket_color = new_rocket_color;
+	}
+
+}
---
+++ Mission Control v1.0/Assets/Rocket_Color_Change.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket_Color_Change : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Start() {		
+		int len = gameObject.GetComponent<MeshRenderer> ().materials.Length;
+
+		//Based on the value held in GameState's rocket-color field, cycle through all the materials in the Mesh
+		//Renderer Component and shift the colors in the appropriate direction, effectively "tinting" the object.
+		if (GameState.rocket_color == "Red") {
+			for (int i = 0; i < len; i++) {
+				Color curr = gameObject.GetComponent<MeshRenderer> ().materials [i].color;
+				curr.r += (float)100 / (float)256;
+				curr.g -= (float)100 / (float)256;
+				curr.b -= (float)100 / (float)256;
+				gameObject.GetComponent<MeshRenderer> ().materials [i].SetColor("_Color", curr);
+			}
+		}
+
+		if (GameState.rocket_color == "Green") {
+			for (int i = 0; i < len; i++) {
+				Color curr = gameObject.GetComponent<MeshRenderer> ().materials [i].color;
+				curr.r -= (float)100 / (float)256;
+				curr.g += (float)100 / (float)256;
+				curr.b -= (float)100 / (float)256;
+				gameObject.GetComponent<MeshRenderer> ().materials [i].SetColor("_Color", curr);
+			}
+		}
+
+		if (GameState.rocket_color == "Blue") {
+			for (int i = 0; i < len; i++) {
+				Color curr = gameObject.GetComponent<MeshRenderer> ().materials [i].color;
+				curr.r -= (float)100 / (float)256;
+				curr.g -= (float)100 / (float)256;
+				curr.b += (float)100 / (float)256;
+				gameObject.GetComponent<MeshRenderer> ().materials [i].SetColor("_Color", curr);
+			}
+		}
+	
+	}
+}
