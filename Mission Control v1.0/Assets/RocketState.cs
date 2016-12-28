983c4010af29fed93cf7b29ff063577e14e747ed
KRader627 <klr020@bucknell.edu>
2016-12-28 18:06:14 -0500

    Font Changes and Slider Functionality
    Made some extremely minor changes to the font used throughout the game.
    Added functionality for the sliders to actually do something; you can
    see their values in the ScriptManager, and they update accordingly. Made
    some notes about rocket choices for the missions in the Google Drive.

---
+++ Mission Control v1.0/Assets/RocketState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RocketState : MonoBehaviour {
+
+	public float fuel = 100;
+	public float angle = 180;
+
+	const float max_fuel = 100;
+	const float max_angle = 180;
+
+	void Update() {
+		set_fuel (GameObject.Find ("Fuel Slider").GetComponent<Slider> ().value);
+		set_angle (GameObject.Find ("Angle Slider").GetComponent<Slider> ().value);
+	}
+
+	public float get_fuel() {
+		return fuel;
+	}
+
+	public void set_fuel(float new_fuel) {
+		fuel = max_fuel * new_fuel;
+	}
+
+	public float get_angle() {
+		return angle;
+	}
+
+	public void set_angle(float new_angle) {
+		angle = max_angle * new_angle;
+	}
+
+}
